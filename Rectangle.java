862c6cb9d9884d4fc12fea6a9a403eec3ae518f1
rehana938 <mantinoonerehana934@gmail.com>
2025-08-01 16:14:22 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the height of rectangle:");
+		int h=sc.nextInt();
+		System.out.println("enter the base of rectangle:");
+		int b=sc.nextInt();
+		System.out.println("area of the rectangle is:"+(0.5)*b*h);
+
+	}
+
+}
