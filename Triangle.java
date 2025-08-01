9feb8ac997f2c8fb8b8487df05fd11735e8281e5
rehana938 <mantinoonerehana934@gmail.com>
2025-08-01 16:14:40 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int length=6;
+		int breadth=4;
+		int area;
+		area=length*breadth;
+		System.out.println(" Area of Triangle is:"+area);
+
+	}
+
+}
