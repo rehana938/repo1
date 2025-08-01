64a2d0e9d494b4f5ddb6873f189c8084f6d60e87
rehana938 <mantinoonerehana934@gmail.com>
2025-08-01 16:14:05 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,13 @@
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+         int side=5;
+        int area=side*side;
+        System.out.println( "Area of square is: "+area);
+
+
+	}
+
+}
