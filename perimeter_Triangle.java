1283521eb3d0f706099ed8e3873a20e6e454fa7b
rehana938 <mantinoonerehana934@gmail.com>
2025-08-01 16:13:44 +0530

    Add files via upload

---
+++ perimeter_Triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class perimeter_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the perimeter side s1:");
+		int s1=sc.nextInt();
+		System.out.println("enter the perimeter side s2:");
+		int s2=sc.nextInt();
+		System.out.println("enter the perimeter side s3:");
+		int s3=sc.nextInt();
+		int perimeter=s1+s2+s3;
+		System.out.println("perimeter of triangle:"+perimeter);
+		
+		
+
+	}
+
+}
