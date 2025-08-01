38730af8dbbd3ed25a5cf576a6987b14d7d18b82
rehana938 <mantinoonerehana934@gmail.com>
2025-08-01 16:13:25 +0530

    Add files via upload

---
+++ Perimeter_Square.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Perimeter_Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the perimeter Side:");
+		int s=sc.nextInt();
+	int area=(4*s);
+	System.out.println(" area of the perimeter is:"+area);
+
+	}
+
+}
